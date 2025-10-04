9c1db155ed7086c73aa5820a27dcc50ef0fa033b
ni3rav <mahetan35@gmail.com>
2025-10-04 09:58:56 +0530

    add email password auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,3 +2,28 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: "http://localhost:3000",
 });
+
+export async function signIn({
+  data,
+}: {
+  data: { email: string; password: string };
+}) {
+  const res = await authClient.signIn.email({
+    email: data.email,
+    password: data.password,
+  });
+  return res;
+}
+
+export async function signUp({
+  data,
+}: {
+  data: { email: string; password: string; name: string };
+}) {
+  const res = await authClient.signUp.email({
+    email: data.email,
+    password: data.password,
+    name: data.name,
+  });
+  return res;
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,11 +5,8 @@ import * as schema from "@/db/schema";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
+  emailAndPassword: {
+    enabled: true,
   },
   database: drizzleAdapter(db, {
     provider: "pg",
